1c28e7f911bb3a4f8350c65a9cf3379bfcbf453f
Leandro <leandro.mos.l11@gmail.com>
2022-06-21 09:00:54 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Projeto que encontrei no YouTube que consiste em uma versão simples do clássico 
+Super Mário em JavaScript. Decidi por faze-lô para treinar os conceitos vistos 
+na sessão de JS do curso de desenvolvimento web. Adicionei um botão de restart 
+que não tinha no projeto original. Pretendo adicionar um contador em breve.
---
+++ js/logica.js
@@ -0,0 +1,34 @@
+const mario = document.querySelector(".mario");
+const tubo = document.querySelector(".tubo");
+
+// Gatilho comando pulo
+const pulo = () => {
+    mario.classList.add("pulo")
+    setTimeout(() => {
+        mario.classList.remove("pulo");
+    }, 500)
+}
+
+// Verificação pulo
+const loop = setInterval(() => {
+    const posicaoTubo = tubo.offsetLeft;
+    const alturaMario = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+
+    // Verificação colisão horizontal e altura
+    if (posicaoTubo <= 125 && posicaoTubo > 0 && alturaMario < 80) {
+        tubo.style.animation = "none";
+        tubo.style.left = posicaoTubo + "px";
+
+        mario.style.animation = "none";
+        mario.style.bottom = alturaMario + "px";
+
+        mario.src = "img/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
+    }
+}, 10)
+
+document.addEventListener("keydown", pulo);
